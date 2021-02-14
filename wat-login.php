c7b1436a02b83f7f2f4025042cd9f736ea1334ab
Jafran <exjafran@gmail.com>
2021-02-14 16:15:41 +0600

    renamed again directory

--- wat-login.php
+++ wat-login.php
@@ -18,7 +18,7 @@
 defined( 'ABSPATH' ) or die('Direct Script not Allowed');
 define('_WAT', __FILE__);
 
-require_once __DIR__  . '/inc/class.wat.php';
+require_once __DIR__  . '/class/wat.php';
 
 
 // initializing plugin 
